9cd00b1ef5b8a8b1c492d636bfc1b2243b557860
Hasan1284 <hasansevindik1284@gmail.com>
2020-04-24 15:15:04 +0200

    sweet alert denemesi yapıldı

---
+++ Desktop/sweetalert2/app.js
@@ -0,0 +1,56 @@
+/*
+function hata(params) {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: '<a href>Why do I have this issue?</a>'
+    })
+}
+
+function dogru(params) {
+    Swal.fire(
+        'Good job!',
+        'You clicked the button!',
+        'success'
+    )
+}
+
+
+
+function bilgi(params) {
+    Swal.fire({
+        title: '<strong>HTML <u>example</u></strong>',
+        icon: 'info',
+        html: 'You can use <b>bold text</b>, ' +
+            '<a href="//sweetalert2.github.io">links</a> ' +
+            'and other HTML tags',
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText: '<i class="fa fa-thumbs-up"></i> Great!',
+        confirmButtonAriaLabel: 'Thumbs up, great!',
+        cancelButtonText: '<i class="fa fa-thumbs-down"></i>',
+        cancelButtonAriaLabel: 'Thumbs down'
+    })
+}
+
+
+
+document.querySelector("#dogru").addEventListener("click", dogru)
+document.querySelector("#hata").addEventListener("click", hata)
+*/
+
+function bilgi(params) {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Your work has been saved',
+        showConfirmButton: false,
+        timer: 1500
+      })
+}
+
+
+
+document.querySelector("#bilgi").addEventListener("click", bilgi)
